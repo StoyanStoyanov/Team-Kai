9974daba49e8fae6653a38039be757a39e57be13
AleksandarMironov <aleksandar.n.mironov@gmail.com>
2016-06-22 19:36:55 +0300

    Add missing files

---
+++ KaiFighter/KaiFighterGame/EntryPoint.cs
@@ -0,0 +1,24 @@
+﻿namespace KaiFighterGame
+{
+    using System;
+
+#if WINDOWS || LINUX
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class EntryPoint
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            using (var game = new KaiFighterGame())
+            {
+                game.Run();
+            }   
+        }
+    }
+#endif
+}
