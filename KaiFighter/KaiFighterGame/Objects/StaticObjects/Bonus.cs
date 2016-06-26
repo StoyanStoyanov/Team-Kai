297a71adbc1c7a8d2614330d1cfdf10d9f62bca7
An4o <reginamagis@gmail.com>
2016-06-26 19:11:54 +0300

    4 classes
    added classes Bonus, Bounty, Obstacle, Wall

---
+++ KaiFighter/KaiFighterGame/Objects/StaticObjects/Bonus.cs
@@ -0,0 +1,34 @@
+﻿using KaiFighterGame.Utilities;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace KaiFighterGame.Objects.StaticObjects
+{
+    public class Bonus : StaticObject
+    {
+        public Bonus(Vector2 position, ObjectType objectType, string[] resources = null) 
+            : base(position, objectType, resources)
+        {
+        }
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void LoadObject(Texture2D texture)
+        {
+            throw new NotImplementedException();
+        }
+
+     
+        public override void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
